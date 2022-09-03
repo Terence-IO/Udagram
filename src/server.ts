2f9b6a06705d64b2e2cab54a3c114c28b85d3ffd
Terence <112216771+Terence-IO@users.noreply.github.com>
2022-09-03 20:34:36 +0300

    Redo TODO1

--- src/server.ts
+++ src/server.ts
@@ -30,6 +30,10 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
   app.get( "/filteredimage", async (req, res) => {
     let image_url = req.query.image_url.toString;
+
+    if(!image_url){
+      return res.status(400). send("Please input image URL")
+    }
     
     const filteredpath = await filterImageFromURL(image_url);
 
